9360db37dd5c2289721509cc2a25a81203bc1415
Igor Kalinin <tltkalinin@gmail.com>
2025-11-17 11:43:13 +0300

    Удалил задублированные файлы

--- static/js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loginButton = document.getElementById('loginButton');
-    if (loginButton) {
-        loginButton.addEventListener('click', function() {
-            window.location.href = '/auth';
-        });
-    }
-});
